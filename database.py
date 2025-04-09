d255c7ea70d5b5930ee1c76370d3e7e0abcc07c7
Владислав Мякишев <geninkirill526@gmail.com>
2025-04-09 19:39:50 +0300

    start

--- database.py
+++ database.py
@@ -1,35 +1,15 @@
-import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-# sqlite connection
-#SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+# Строка подключения к PostgreSQL
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:yourpassword@localhost:5432/todoapp"
 
-# postgres connection
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:12345@localhost/postgres"
+# Создание движка
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-# postgres connection  heroku
-
-"""HOST_DB_HEROKU = os.environ.get('HOST_DB_HEROKU')
-DB_HEROKU = os.environ.get('DB_HEROKU')
-DB_USER_HEROKU = os.environ.get('DB_USER_HEROKU')
-DB_PASSWORD_HEROKU = os.environ.get('DB_PASSWORD_HEROKU')
-
-SQLALCHEMY_DATABASE_URL = "postgresql://"+DB_USER_HEROKU+":"+DB_PASSWORD_HEROKU +"@"+HOST_DB_HEROKU+":5432/"+DB_HEROKU+""
-"""
-
-
-# mysql connection
-#SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:test1234!@127.0.0.1:3306/todoapp"
-
-engine = create_engine(
-    #solo para sqlite
-    #SQLALCHEMY_DATABASE_URL, connect_args = {"check_same_thread": False}
-   
-    SQLALCHEMY_DATABASE_URL
-)
-
+# Создание сессии
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# Базовый класс для моделей
 Base = declarative_base()
